954017c4119a4f0ce48587934111b58a643e3600
Himanshu Kumar <himanshuy953@gmail.com>
2025-08-18 10:14:40 +0530

    Updated CORS config to allow Vercel frontend

--- src/main/java/com/mangodesk/sumarizer/config/CorsConfig.java
+++ src/main/java/com/mangodesk/sumarizer/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // frontend
+                        .allowedOrigins("https://meeting-summarizer-tau.vercel.app/") // frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
